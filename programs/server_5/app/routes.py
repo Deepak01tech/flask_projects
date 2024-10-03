74587eb6e99cc4316b43645748d99ad6a31e4dc5
Deepak <dp55954@gmail.com>
2024-10-03 16:37:43 +0530

    routes blog added

--- programs/server_5/app/routes.py
+++ programs/server_5/app/routes.py
@@ -21,3 +21,28 @@ def login():
         access_token = generate_token(user.id)
         return jsonify({"access_token": access_token}), 200
     return jsonify({"error": "Invalid email or password"}), 401
+
+
+from bson import ObjectId
+
+
+@main.route('/blog', methods=['GET'])
+@token_required
+def get_blogs(current_user):
+    blogs = Blog.get_all_product()
+    blogs_list = [{**blog,"id":str(blog["_id"])} for blog in blogs]
+    return jsonify(blogs_list),200
+
+
+
+@main.route('/blog/<blog_id>', methods=['GET'])
+@token_required
+def get_blog(current_user,blog_id):
+    blog = Product.get_product_by_id(blog_id)
+    if not blog:
+        return jsonify({"message": "Blog not found"}), 404
+    blog['_id']= str(blog['_id'])
+    return jsonify(blog),200
+
+
+
